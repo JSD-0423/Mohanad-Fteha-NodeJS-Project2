e4f63646e2d16ebe4b500d19467288d8281f01ec
mohanadft <mohanadfteha@gmail.com>
2023-05-29 19:39:00 +0300

    Setup project

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const fileUtils = require('./utils/fileUtils')
+const pug = require('pug')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', path.resolve('./views'))
+
+const BOOKS_FILE = 'books.json'
+
+app.get('/books', (req, res) => {
+	try {
+		const books = fileUtils.readJSONFile(BOOKS_FILE)
+		res.render('books', { books })
+	} catch (error) {
+		res.status(500).send('Invalid book file')
+	}
+})
+
+app.listen(8080, () => {
+	console.log('Server is running')
+})
---
+++ utils/fileUtils.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+function readJSONFile(filePath) {
+	const relativePath = path.resolve('data', filePath)
+
+	if (!fs.existsSync(relativePath)) {
+		fs.writeFileSync(relativePath, '[]', 'utf8')
+		return []
+	}
+
+	const fileContents = fs.readFileSync(relativePath, 'utf8')
+	return JSON.parse(fileContents)
+}
+
+module.exports = {
+	readJSONFile
+}
