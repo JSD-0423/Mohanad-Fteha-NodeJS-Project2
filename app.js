31fa8aac0ab2c43bd3c06d6547bf9e5297069ca4
mohanadft <mohanadfteha@gmail.com>
2023-05-31 17:04:21 +0300

    Fix: inovke body parser middleware correctlry

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.json({}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'pug')
 app.set('views', path.resolve('./views'))
 
